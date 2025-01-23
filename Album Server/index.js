a2f256bed0232e1adc6db5e7459fb746638978bd
ashiyaman <ashiya1005@gmail.com>
2025-01-23 16:38:25 +0530

    update car using routes

---
+++ Album Server/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const albums = [
+
+    { id: 1, title: 'Abbey Road', artist: 'The Beatles', year: 1969 },
+  
+    { id: 2, title: 'The Dark Side of the Moon', artist: 'Pink Floyd', year: 1973 },
+  
+    { id: 3, title: 'Thriller', artist: 'Michael Jackson', year: 1982 }
+  
+  ];
+
+app.get('/', (req, res) => {
+    res.send('Hello, This is Express Assignment Server.')
+})
+
+app.post('/albums', (req, res) => {
+    const newAlbum = req.body
+
+    if(!newAlbum.title || !newAlbum.artist || !newAlbum.year){
+        res.status(400).json({error: 'Title, artist and year are required.'})
+    }
+    else{
+        albums.push(newAlbum)
+        res.status(200).json({message: 'Album added successfully.', album: newAlbum})
+    }
+})
+
+app.delete('/albums/:id', (req, res) => {
+    const albumToBeDeletedId = parseInt(req.params.id)
+
+    const albumIndex = albums.findIndex((album) => album.id === albumToBeDeletedId)
+
+    if(albumIndex === -1){
+        res.status(404).json({error: 'Album not found.'})
+    }
+    else{
+        albums.splice(albumIndex, 1)
+        res.status(200).json({message: 'Album deleted successfully.'})
+    }
+})
+
+app.post('/albums', (req, res) => {
+    
+})
+
+app.get('/albums', (req, res) => {
+    res.send(albums)
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('Connected to Server')
+})
---
+++ Update using Routes-1/index (2).js
@@ -0,0 +1,57 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const cars = [
+    {id: 1, make: "Toyota", model: "Camry", year: 2022},
+    {id: 2, make: "Honda", model: "Civic", year: 2022},
+    {id: 3, make: "Ford", model: "Mustang", year: 2022},
+    {id: 4, make: "Tesla", model: "Model 3", year: 2022},
+    {id: 5, make: "Audi", model: "A 4", year: 2022},
+    {id: 6, make: "Kia", model: "Seltos", year: 2022},
+    {id: 8, make: "Volkswagen", model: "Polo", year: 2022},
+    {id: 9, make: "Hyundai", model: "Tuscan", year: 2022},
+    {id: 10, make: "Mercedes-Benz", model: "C-Class", year: 2022},
+    {id: 11, make: "Nissan", model: "Altima", year: 2022},
+    {id: 12, make: "BMW", model: "X5", year: 2022},
+]
+
+app.get("/", (req, res) => {
+    res.send("Hello, Express.")
+})
+
+app.post("/cars", (req, res) => {
+    const newCar = req.body
+
+    if(!newCar.make || !newCar.model || !newCar.year){
+        res.status(400).json({error: "Make, model and year are required"})
+    }
+    else{
+        cars.push(newCar)
+        res.status(201).json({message: "Car added successfully", car: newCar})
+    }
+})
+
+app.get("/cars", (req, res) => {
+    res.send(cars)
+})
+
+app.delete("/cars/:id", (req, res) => {
+    const carId = parseInt(req.params.id)
+
+    const index = cars.findIndex((car) => car.id === carId)
+
+    if(index === -1){
+        res.status(404).json({error: "Car Not Found"})
+    }
+    else{
+        cars.splice(index, 1)
+        res.status(200).json({message: "Car deleted Successfully."})
+    }
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log("Server is running on port", PORT)
+})
