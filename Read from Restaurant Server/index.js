b4ed3e8ae51cbac4fdf90079efbfe348ce09847c
ashiyaman <ashiya1005@gmail.com>
2025-01-24 16:19:34 +0530

    read from restaurant server using express and mongo

--- Read from Restaurant Server/index.js
+++ Read from Restaurant Server/index.js
@@ -1,71 +1,13 @@
 const mongoose = require("mongoose")
 const Restaurants = require("./models/restaurants.models")
+const express = require('express')
 
 const {initializeDatabase} = require("./db/db.connect")
+
+const app = express()
+
+app.use(express.json())
 initializeDatabase()
-
-/*const newRestaurant = {
-    name: "Cha Cha",
-    cuisine: ["Spanish"],
-    location: "123 Main Street, Anytown",
-    rating: 4.0,
-    reviews: [],
-    website: "https://example.com",
-    phoneNumber: "+1234567890",
-    openHours: "Mon-Sun: 11:00 AM - 10:00 PM",
-    priceRange: "$$ (11-30)",
-    reservationsNeeded: true,
-    isDeliveryAvailable: true,
-    menuUrl: "https://example.com/menu",
-    photos: ["https://example.com/photo1.jpg", "https://example.com/photo2.jpg"],
-  };*/
-
-  /*const newRestaurant = {
-    name: "Somi",
-    cuisine: ["Greek"],
-    location: "11 Main Road, Gem",
-    rating: 4.3,
-    reviews: [],
-    website: "https://somi-example.com",
-    phoneNumber: "+1234997390",
-    openHours: "Tue-Sun: 11:00 AM - 10:00 PM",
-    priceRange: "$$ (11-30)",
-    reservationsNeeded: false,
-    isDeliveryAvailable: true,
-    menuUrl: "https://somi-example.com/menu",
-    photos: ["https://example.com/somi-photo1.jpg", "https://example.com/somi-photo2.jpg"],
-  };*/
-
-  const newRestaurant = {
-    name: "Yo China",
-    cuisine: ["Chinese", "Italian"],
-    location: "MG Road, Bangalore",
-    rating: 3.9,
-    reviews: [],
-    website: "https://yo-example.com",
-    phoneNumber: "+1288997392",
-    openHours: "Tue-Sun: 10:00 AM - 11:00 PM",
-    priceRange: "$$$ (31-60)",
-    reservationsNeeded: true,
-    isDeliveryAvailable: false,
-    menuUrl: "https://yo-example.com/menu",
-    photos: ["https://example.com/yo-photo1.jpg", "https://example.com/yo-photo2.jpg", "https://example.com/yo-photo3.jpg"]
-  };
-
-  //Save new restaurant to database
-
-  async function createRestaurant(newRestaurant) {
-    try{
-        const restaurant = new Restaurants(newRestaurant)
-        await restaurant.save()
-        console.log("Restaurant saved")
-    } 
-    catch(error){
-        throw error
-    }
-}
-
-//createRestaurant(newRestaurant)
 
 //Get all the restaurants from database
 
@@ -73,69 +15,78 @@ const readAllRestaurants = async() => {
     try{
         const restaurants = await Restaurants.find()
         console.log(restaurants)
+        return restaurants
     }
     catch(error){
         throw error
     }
 }
 
-//readAllRestaurants()
+app.get('/restaurants', async (req, res) => {
+    try{
+        const restaurants = await readAllRestaurants()
+        res.send(restaurants)
+    }
+    catch{
+        res.status(500).json({error: 'Error while fetching restaurants.'})
+    }
+})
 
 //Get a restauant with a particular name
 
 const readRestaurantByName = async(restaurantName) => {
     try{
         const restaurant = await Restaurants.findOne({name: restaurantName})
+        
+    }
+    catch(error){
+        throw error
+    }
+}
+
+app.get('/restaurants/:restaurantName', async (req, res) => {
+    try{
+        const restaurant = await readRestaurantByName(req.params.restaurantName)
         console.log(restaurant)
+        if(restaurant){
+            res.send(restaurant)
+        }
+        else{
+            res.status(404).json({error: 'Restaurant not found.'})
+        }
     }
-    catch(error){
-        throw error
+    catch{
+        res.status(500).json({error: 'Error while fetching restaurant.'})
     }
-}
-
-//readRestaurantByName("Somi")
-
-//Get all restaurants which has reservations
-
-const readAllRestaurantsWithReservation = async() => {
-    try{
-        const restaurantsWithReservation = await Restaurants.find({reservationsNeeded: true})
-        console.log(restaurantsWithReservation)
-    }
-    catch(error){
-        throw error
-    }
-}
-
-//readAllRestaurantsWithReservation()
-
-//Get all restaurants which has delivery
-
-const readAllRestaurantsWithDelivery = async() => {
-    try{
-        const restaurantsWithDelivery = await Restaurants.find({isDeliveryAvailable: true})
-        console.log(restaurantsWithDelivery)
-    }
-    catch(error){
-        throw error
-    }
-}
-
-//readAllRestaurantsWithDelivery()
+})
 
 //Get a restaurant by a particlar phone no.
 
 const readRestaurantByPhoneNo = async(restaurantPhoneNumber) => {
     try{
         const restaurant = await Restaurants.findOne({phoneNumber: restaurantPhoneNumber})
-        console.log(restaurant)
+        return restaurant
     }
     catch(error){
         throw error
     }
 }
 
-//readRestaurantByPhoneNo("+1288997392")
+app.get('/restaurants/directory/:phoneNumber', async (req, res) => {
+    try{
+        const restaurant = await readRestaurantByPhoneNo(req.params.phoneNumber)
+        console.log(restaurant)
+        if(restaurant){
+            res.send(restaurant)
+        }
+        else{
+            res.status(404).json({error: 'Restaurant not found.'})
+        }
+    }
+    catch{
+        res.status(500).json({error: 'Error while fetching restaurant.'})
+    }
+})
 
 //Get all restaurants with particular cuisine
 
@@ -143,10 +94,59 @@ const readAllRestaurantsByCuisine = async(cuisine) => {
     try{
         const restaurant = await Restaurants.find({cuisine: cuisine})
         console.log(restaurant)
+        return restaurant
     }
     catch(error){
         throw error
     }
 }
 
-readAllRestaurantsByCuisine("Italian")
+app.get('/restaurants/cuisine/:cuisineName', async (req, res) => {
+    try{
+        const restaurant = await readAllRestaurantsByCuisine(req.params.cuisineName)
+        console.log(restaurant)
+        if(restaurant){
+            res.send(restaurant)
+        }
+        else{
+            res.status(404).json({error: 'Restaurant not found.'})
+        }
+    }
+    catch{
+        res.status(500).json({error: 'Error while fetching restaurant.'})
+    }
+})
+
+//Get all restaurants with particular location
+
+const readAllRestaurantsByLocation = async(location) => {
+    try{
+        const restaurant = await Restaurants.find({location: location})
+        console.log(restaurant)
+        return restaurant
+    }
+    catch(error){
+        throw error
+    }
+}
+
+app.get('/restaurants/location/:restaurantLocation', async (req, res) => {
+    try{
+        const restaurant = await readAllRestaurantsByLocation(req.params.restaurantLocation)
+        console.log(restaurant)
+        if(restaurant){
+            res.send(restaurant)
+        }
+        else{
+            res.status(404).json({error: 'Restaurant not found.'})
+        }
+    }
+    catch{
+        res.status(500).json({error: 'Error while fetching restaurant.'})
+    }})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log('App running on port', PORT)
+})
+
